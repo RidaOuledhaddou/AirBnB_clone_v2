7dfe42df7ebfa79ec81266b42ca92872a1744c83
RidaOuledhaddou <ouledhaddourida@gmail.com>
2024-04-26 02:55:26 -0700

    AirBnB_clone_v2

--- main_delete.py
+++ main_delete.py
@@ -41,7 +41,7 @@ for state_key in all_states.keys():
 # Delete the new State
 fs.delete(new_state)
 
-# All States
+# All States  
 all_states = fs.all(State)
 print("All States: {}".format(len(all_states.keys())))
 for state_key in all_states.keys():
